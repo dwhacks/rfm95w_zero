b604cb7e348b569e5d2c892a68935b3cd054b991
dwhacks <forsakenrider@hotmail.com>
2025-11-18 18:49:09 -0800

    Initial commit

---
+++ variants/rfm95w_zero/variant.h
@@ -0,0 +1,34 @@
+// Pin definitions to match schematics drawing
+#define PIN_LED_DEBUG 1
+#define PIN_CS_ACCESSORY 2
+#define PIN_I2C0_SCL 5
+#define PIN_I2C0_SDA 4
+#define PIN_SPI_SCK 10
+#define PIN_SPI_MOSI 11
+#define PIN_SPI_MISO 12
+#define PIN_SPI_CS 13
+#define PIN_RFM_IRQ 14
+#define PIN_RFM_RESET 15
+#define PIN_UART_TX 43
+#define PIN_UART_RX 44
+// End pin definitions
+
+#define I2C_SDA PIN_I2C0_SDA // I2C pins for this board
+#define I2C_SCL PIN_I2C0_SCL
+
+#define LED_PIN PIN_LED_DEBUG // If defined we will blink this LED
+
+#define ARDUINO_ARCH_AVR
+
+#define HAS_CPU_SHUTDOWN 1
+
+#define USE_RF95
+#define LORA_SCK PIN_SPI_SCK
+#define LORA_MISO PIN_SPI_MISO
+#define LORA_MOSI PIN_SPI_MOSI
+#define LORA_CS PIN_SPI_CS
+#define LORA_DIO0 PIN_RFM_IRQ
+#define LORA_RESET PIN_RFM_RESET
+
+#define LORA_DIO1 RADIOLIB_NC
+#define LORA_DIO2 RADIOLIB_NC
